2d3c7377337bc8f0161ac102d52e190cc8ac02f9
tamar-nisan <tamarnisan@hilms.tech>
2024-11-13 15:42:33 +0200

    posts

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -2,28 +2,84 @@
 import React from 'react'
 
 import { useState, useEffect } from 'react'
+import Post from './Post'
 
 export default function Posts() {
-    const current= JSON.parse(localStorage.getItem("currentUser"));
-    const id=current.id;
+    const [id, setId] = useState("");
+    const [title, setTitle] = useState("");
+    const [bodynew, setBodynew] = useState("")
+    const [body, setBody] = useState("")
+    const [data, setData] = useState([]);
+    const [add, setAdd] = useState(false)
+    const current = JSON.parse(localStorage.getItem("currentUser"));
+    const userId = current.id;
     useEffect(() => {
         async function getPosts(url) {
             try {
                 let response = await fetch(url);
                 if (!response.ok) throw Error("Did not recived expected data")
-                let data = await response.json();
-            console.log(data)
-               
+                setData(await response.json());
+
             }
             catch (error) {
                 alert(error)
             }
 
         };
-        (async () => await getPosts(`http://localhost:3500/posts/${id}`))()
+        (async () => await getPosts(`http://localhost:3500/posts?userId=${userId}`))()
 
     }, [])
-      return (
- <></>
-  )
+    function AddPost(event) {
+        event.preventDefault()
+        let obj = {"userId":userId, "id": Math.floor(Math.random() * (10000 - 10) + 10), "title": title, "body": bodynew }
+       setData(prev => [...prev, obj]);
+
+       setAdd(false);
+       
+       fetch('http://localhost:3500/posts', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error posting data:', error));
+
+    }
+    return (
+        <>
+        <h1>Your Posts:</h1>
+            <button onClick={()=>setAdd(true)}>ADD POST</button>
+            {add &&
+                <form onSubmit={AddPost}>
+                    <label>Enter title:
+                        <input
+                            type="text"
+                            value={title}
+                            onChange={(e) => {
+                                setTitle(e.target.value)
+                            }}
+                        />
+                    </label>
+                    <label>Enter body:
+                        <input
+                            type="text"
+                            value={bodynew}
+                            onChange={(e) => {
+                                setBodynew(e.target.value)
+                            }}
+                          
+                        />
+                    </label>
+                    <input type="submit" />
+                </form>
+            }
+            {data.map((post, i) => {return <Post post={post} i={i} arr={data} setArr={setData} key={post.id} />
+}
+            )}
+
+        </>
+    );
 }
