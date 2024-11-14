8e594ad424caf59db850807ad058ef1ea43e2717
tamar-nisan <tamarnisan@hilms.tech>
2024-11-14 09:16:16 +0200

    logout

--- src/HomeNavBar.jsx
+++ src/HomeNavBar.jsx
@@ -5,10 +5,18 @@ import { useNavigate } from "react-router-dom";
 
 
 export default function HomeNavBar() {
+  function componentDidUpdate() {
+    window.history.pushState(null, document.title, window.location.href);
+    window.addEventListener('popstate', function(event) {
+      window.history.pushState(null, document.title, window.location.href);
+    });
+  }
     const navigate = useNavigate();
     function onclickLogout() {
         localStorage.setItem("currentUser", {});
         navigate("/");
+        componentDidUpdate();
+        
       }
       console.log("hi")
   return (
