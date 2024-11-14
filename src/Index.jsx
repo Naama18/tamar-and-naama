fc2a78f317b6f9420dd93279562686f95bb65853
tamar-nisan <tamarnisan@hilms.tech>
2024-11-14 09:31:50 +0200

    add

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import LogIn from "./LogIn";
 import Resgister from "./Register";
+import "./index.css"
 
 export default function Index() {
   const [displayRegister, setDisplayRegister] = useState(false);
   const [displayLogIn, setDisplayLogIn] = useState(false);
   return (
-    <>
+   
+    <div class="indexContainer">
       <h1>Welcome to our Website!</h1>
-      <button onClick={() => setDisplayLogIn(true)}>Log In</button>
+      <button onClick={() => {setDisplayLogIn(true);
+        setDisplayRegister(false)}}>Log In</button>
       {displayLogIn && <LogIn />}
-      <button onClick={() => setDisplayRegister(true)}>Register</button>
+      <button onClick={() => {setDisplayRegister(true);
+      setDisplayLogIn(false)}
+
+      }>Register</button>
       {displayRegister && <Resgister />}
-    </>
+      </div>
+  
   );
 }
